a6b1906e3db6b02fd6deaca592dc68f1a150846d
Jason Bell <jabell4610@gmail.com>
2018-06-06 12:28:42 -0400

    Adding routes, views, and controller logic

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,30 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Helpers\MbtaApi;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function showRoutes() {
+        // Execute API call to retrieve all route information
+        $helper = new MbtaApi\Routes();
+        $response = $helper->request('get');
+        $responseData = json_decode($response->getBody());
+
+        // Send responseData to the view to build out the tables
+        return view('welcome', array('routeData' => $responseData));
+
+    }
+    public function showSchedule($request)
+    {
+        // Execute API call to retrieve route information
+        $helper = new MbtaApi\Schedule();
+        $response = $helper->request('get', array(), array('[route]' => $request));
+        $responseData = json_decode($response->getBody());
+
+        // Send responseData to the view to build out schedule table
+        return view('schedule', array('scheduleData' => $responseData));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Controller@showRoutes');
+Route::get('/schedule/{scheduleId}', 'Controller@showSchedule');
+
+
+
